188a84663e293f26ff1db41e6b2ce0ffae6202ca
Anton Tarasenko <report641@gmail.com>
2022-11-18 22:25:37 +0700

    Реализовано редактирование масштаба изображения

--- js/form.js
+++ js/form.js
@@ -1,7 +1,12 @@
 import { isEscapeKeyPress } from './utils.js';
+import { onZoomOutButtonClick } from './photo_scale_changer.js';
+import { onZoomInButtonClick } from './photo_scale_changer.js';
+import { resetScaleValue } from './photo_scale_changer.js';
 const fileInput = document.querySelector('.img-upload__input');
 const uploadOverlay = document.querySelector('.img-upload__overlay');
 const closeButton = document.querySelector('.img-upload__cancel');
+const controlSmaller = document.querySelector('.scale__control--smaller');
+const controlBigger = document.querySelector('.scale__control--bigger');
 
 const onEscapeKeyDown = (evt)=> {
 
@@ -20,6 +25,8 @@ const closeEditPictureModal = () => {
   uploadOverlay.classList.add('hidden');
   document.removeEventListener('keydown', onEscapeKeyDown);
   closeButton.removeEventListener('click', onClickCloseButton);
+  controlSmaller.removeEventListener('click', onZoomOutButtonClick);
+  controlBigger.removeEventListener('click', onZoomInButtonClick);
   fileInput.value = '';
 };
 
@@ -28,6 +35,9 @@ const openEditPictureModal = () => {
   uploadOverlay.classList.remove('hidden');
   closeButton.addEventListener('click', onClickCloseButton);
   document.addEventListener('keydown', onEscapeKeyDown);
+  controlSmaller.addEventListener('click', onZoomOutButtonClick);
+  controlBigger.addEventListener('click', onZoomInButtonClick);
+  resetScaleValue();
 };
 
 fileInput.addEventListener('change', ()=> {
